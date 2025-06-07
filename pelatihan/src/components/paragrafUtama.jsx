8ffb32416ed2c7721d6a48c89fa227e2b1e5ddd5
farrel ekaputra <farrelekaputra1@gmail.com>
2025-06-07 23:54:59 +0700

    revisi minor

--- pelatihan/src/components/paragrafUtama.jsx
+++ pelatihan/src/components/paragrafUtama.jsx
@@ -1,6 +1,6 @@
 const mainText = ({teks}) => {
     return(
-        <section className="text w-full justify-center mt-4 md:p-4">
+        <section className="text w-full justify-center mt-4 md:p-4 gap-1">
         {teks.map((paragraph, index) => (
             <p key={index} 
                 className="w-full md:w-3/4 mx-auto flex-wrap text-justify text-gray-700 text-lg/8 gap-y-2" dangerouslySetInnerHTML={{__html:paragraph}}/>
