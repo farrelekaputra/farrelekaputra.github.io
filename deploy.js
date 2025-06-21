b9649285a83ee36600707ed3ffc4239682472841
farrelekaputra <128167479+farrelekaputra@users.noreply.github.com>
2025-06-21 23:22:43 +0700

    masih eror

--- deploy.js
+++ deploy.js
@@ -1,13 +1,13 @@
-const ghpages = require('gh-pages');
+import ghpages from 'gh-pages';
 
 ghpages.publish('dist', {
   branch: 'gh-pages',
   repo: 'https://github.com/farrelekaputra/farrelekaputra.github.io.git',
-  message: 'Deploy React App via Ai-chan ✨',
-}, function (err) {
+  message: 'Deploy dari Ai-chan~ ✨',
+}, (err) => {
   if (err) {
-    console.error('Deploy GAGAL (ಥ﹏ಥ):', err);
+    console.error('Gagal deploy:', err);
   } else {
-    console.log('Deploy sukses! ✨🥳✨');
+    console.log('BERHASIL! Yay!');
   }
 });
