8f5821d4805425a6fe009286a0c8de85f2d2399a
farrelekaputra <128167479+farrelekaputra@users.noreply.github.com>
2025-07-11 09:55:40 +0700

    coba lagi

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import updateMe from './assets/updateMe.png'
+import learn from './assets/new.png'
+import programming from './assets//programming.png'
+import room from './assets/room.png'
+import esc from './assets/portofolio/esc.png'
+import zakat from './assets/portofolio/Zakatify.png'
+import arcaloka from './assets/portofolio/arcaloka.png'
+import separo from './assets/portofolio/separo.png'
+import jtravel from './assets/portofolio/1.png'
+
+import magang from './assets/blogMaterial/magang.jpg'
+import keyboard from './assets/blogMaterial/keebs.jpg'
+import daredevil from './assets/blogMaterial/matt.png'
+
+export{
+    updateMe,
+    learn,
+    programming,
+    room,
+    esc,
+    zakat,
+    arcaloka,
+    separo,
+    jtravel,
+    magang, 
+    keyboard,
+    daredevil
+}
