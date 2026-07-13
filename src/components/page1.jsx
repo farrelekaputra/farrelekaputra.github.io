e51b020aff3f365f7971a22d7c75c2f11df6c3dd
Farrel Ekaputra <farrelekaputra1@gmail.com>
2026-07-13 11:07:09 +0700

    masih error jit

--- src/components/page1.jsx
+++ src/components/page1.jsx
@@ -39,7 +39,7 @@ const WalkingText = () => {
                             Ui/Ux
                         </div>
                     </div>
-                    <div className="design flex gap-1">
+                    <div className="Design flex gap-1">
                         <div className="icon">
                             <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" viewBox="0 0 14 14">
                                 <path fill="#2563EB" fillRule="evenodd" d="M8.308.922L6.75 3q.07.053.134.116l4 4q.063.063.116.134l2.078-1.558a1 1 0 0 0 .107-1.507L9.815.815a1 1 0 0 0-1.507.107M1.381 5.254L.166 12.95L3.116 10l-.308-.308a.625.625 0 0 1 .884-.884l1.5 1.5a.625.625 0 0 1-.884.884L4 10.884l-2.95 2.95l7.696-1.215a1 1 0 0 0 .838-.877L10 8L6 4l-3.742.416a1 1 0 0 0-.877.838" clipRule="evenodd" />
